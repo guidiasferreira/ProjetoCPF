82b73e77fd94a1d66d631394277723e5ff9e390c
Guilherme Dias Ferreira <gui.ferreira2606@gmail.com>
2025-09-10 10:19:32 -0300

    Arquivo principal, onde o usuário escolhe uma opção e o programa retorna os resultados

--- main.py
+++ main.py
@@ -1,39 +1,56 @@
-import gerar_cpf
-import validar_cpf
+from gerar_cpf import gerar
+from validar_cpf import validar
 from time import sleep
 
+
 def menu():
+    print()
     print("1. Gerar CPF")
     print("2. Validar CPF")
     print("3. Sair do programa")
+    print()
+
+
+def executar_opcao(opcao):
+    match (opcao):
+        case 1:
+            result = gerar()
+        
+        case 2:
+            cpf = str(input("Digite o CPF: ")).replace(".", "").replace("-", "").replace(" ", "")
+
+            if (validar(cpf)):
+                result = "CPF válido! ✅"
+
+            else:
+                result = "CPF inválido! ❌"
+        
+        case 3:
+            return None
+        
+        case _:
+            result = "Escolha uma opção entre 1 e 3."
+    
+    return result
 
 
 while (True):
     menu()
 
     try:
-        opcao = int(input("\nEscolha uma opção: "))
+        opcao = int(input("Escolha uma opção: "))
+        result = executar_opcao(opcao)
 
-        match (opcao):
-            case 1:
-                print(gerar_cpf.gerar())
-            
-            case 2:
-                cpf = str(input("Digite o CPF: ")).replace(".", "").replace("-", "").replace(" ", "")
-
-                if (validar_cpf.validar(cpf)):
-                    print("CPF válido! ✅")
-
-                else:
-                    print("CPF inválido! ❌")
-
-            case 3:
-                print("\nSaindo...")
-                sleep(1)
-                break
-
-            case _:
-                print("Informe um número entre 1 e 3.")
+        if (result is None):
+            print("\nSaindo do programa...")
+            sleep(1)
+            break
+        
+        else:
+            print(result)
     
     except (ValueError):
-        print("\nERRO! Informe apenas um número inteiro (int).")
+        print("\nErro! Informe apenas um número inteiro (int).")
+    
+    except Exception as e:
+        print(f"\nErro! Desconhecido: {e}")
